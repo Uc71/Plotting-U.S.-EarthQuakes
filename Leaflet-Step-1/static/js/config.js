ec47e0831f3b973cb168fcbb6e955f873f211153
William Sargent <wns1997@icloud.com>
2021-03-30 18:34:43 -0400

    commented out quakes code

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoid2lsbHNhcmdlbnQiLCJhIjoiY2ttaHRiOGE0MGFyczJ3bDhnZGdnbXhxbyJ9.SWN7-2sv4UqmRunO0l19wA";
+const API_KEY = "pk.eyJ1Ijoid2lsbHNhcmdlbnQiLCJhIjoiY2ttd2w5bWhtMGcyaTJ2cXAxazY0dWxjbCJ9.PoKn3w0JU4nLqX-1YrGRmQ";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,21 +1,22 @@
-var myMap=L.map("map",{
+var myMap=L.map("mapid",{
     center:[44.967243,-103.771556],
     zoom:13
    });
-   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+var grayMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution:'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom:18,
     id:'mapbox/streets-v11',
     tileSize:512,
     zoomOffset:-1,
     accessToken:API_KEY
-}).addTo(mymap);
-var quakes=d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(data){})
-for(var i=0;i<quakes.length;i++){
-    L.circle(quakes[i].location,{
-     fillOpacity:.75,
-     color:"white",
-     fillColor:"purple",
-     radius:markerSize(quakes[i].mag) 
-     }).bindPopup("<h1>"+quakes[i].time +"</h1> <hr> <h3>Population: "+quakes[i].gap+"</h3>").addTo(myMap);
-    }
+});
+grayMap.addTo(myMap);
+// var quakes=d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(data){})
+// for(var i=0;i<quakes.length;i++){
+//     L.circle(quakes[i].location,{
+//      fillOpacity:.75,
+//      color:"white",
+//      fillColor:"purple",
+//      radius:markerSize(quakes[i].mag) 
+//      }).bindPopup("<h1>"+quakes[i].time +"</h1> <hr> <h3>Population: "+quakes[i].gap+"</h3>").addTo(myMap);
+//     }
