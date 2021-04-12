da1e1af55e5c26ff29c480743209dd17646e4565
William Sargent <wns1997@icloud.com>
2021-04-11 23:40:12 -0400

    added depth-colored markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,10 +1,10 @@
 var myMap=L.map("mapid",{
     center:[44.967243,-103.771556],
-    zoom:13
+    zoom:3
    });
 var grayMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution:'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom:18,
+    maxZoom:10,
     id:'mapbox/streets-v11',
     tileSize:512,
     zoomOffset:-1,
@@ -22,11 +22,19 @@ grayMap.addTo(myMap);
 //     }
     //COPIED FROM EXERCISE 17.2.4
     // Grab data with d3
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(data){
-    function style(){
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson",function(data){
+    console.log(data)
+    function styleI(feature){
+        return {
+          fillColor: getcolor(feature.geometry.coordinates[2]),
+          "color": "black",
+          "fillOpacity": 1,
+          "opacity":1,
+          "stroke":false
+        }
     };
     function getcolor(depth){
-      switch(True){
+      switch(true){
         case depth > 90:
           return "#00FFA4";
         case depth > 70:
@@ -38,53 +46,75 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
         case depth > 10:
           return "#003422";
         default:
-          return "#000000";
+          return "000000";
       }
       };
     function getradius(mag){
       return (mag/Math.PI)**.5;
     };
-    geojson=L.circle(data,{
-      // Define what  property in the features to use
-      valueProperty:"geometry.coordinates",
-      // Set color scale
-      scale: ["#ffffb2", "#b10026"],
-      // Number of breaks in step range
-      steps: 10,
-      // q for quartile, e for equidistant, k for k-means
-      mode: "q",
-      style: {
-        // Border color
-        color: "#fff",
-        weight: 1,
-        fillOpacity: 0.8
-      },
-      // Binding a pop-up to each layer
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
-          "$" + feature.properties.MHI2016);
+
+    console.log(data);
+
+  //   var geojsonMarkerOptions = {
+  //     radius: 8,
+  //     fillColor: getcolor(feature.geometry.coordinates[2]),
+  //     color: "#000",
+  //     weight: 1,
+  //     opacity: 1,
+  //     fillOpacity: 0.8
+  // };
+
+    L.geoJSON(data, {
+      style: styleI,
+      pointToLayer: function(feature, latlng) {
+        //geojsonMarkerOptions.fillColor(feature.geometry.coordinates[2])
+        return L.circleMarker(latlng )
       }
-    }).addTo(myMap);
+  }).addTo(myMap);
+    // geojson=L.circle(data,{
+    //   // Define what  property in the features to use
+    //   valueProperty:"geometry.coordinates",
+    //   // Set color scale
+    //   scale:["#ffffb2","#b10026"],
+    //   // Number of breaks in step range
+    //   steps:10,
+    //   // q for quartile, e for equidistant, k for k-means
+    //   //mode: "q",
+    //   style: {
+    //     // Border color
+    //     color: "#fff",
+    //   },
+    //   // Binding a pop-up to each layer
+    //   onEachFeature: function(feature,layer){
+    //     layer.bindPopup("Zip Code: "+feature.properties.ZIP + "<br>Median Household Income:<br>"+
+    //       "$"+feature.properties.MHI2016);
+    //   }
+    // }).addTo(myMap);
     // Set up the legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = geojson.options.limits;
-      var colors = geojson.options.colors;
-      var labels = [];
-      // Add min & max
-      var legendInfo = "<h1>Median Income</h1>" +
-        "<div class=\"labels\">" +
-          "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
-      div.innerHTML = legendInfo;
-      limits.forEach(function(limit, index) {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-      });
-      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-      return div;
-    };
-    // Adding legend to the map
-    legend.addTo(myMap);
+    // var legend = L.control({ position: "bottomright" });
+    // legend.onAdd = function() {
+    //   var div = L.DomUtil.create("div", "info legend");
+    //   var limits = geojson.options.limits;
+    //   var colors = geojson.options.colors;
+    //   var labels = [];
+    //   // Add min & max
+    //   var legendInfo = "<h1>Median Income</h1>" +
+    //     "<div class=\"labels\">" +
+    //       "<div class=\"min\">" + limits[0] + "</div>" +
+    //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    //     "</div>";
+    //   div.innerHTML = legendInfo;
+    //   limits.forEach(function(limit, index) {
+    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    //   });
+    //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    //   return div;
+    // };
+    // // Adding legend to the map
+    // legend.addTo(myMap);
+
+
+
+    // a = ['red'. 'blue']
+    // b = ['1-3', '3-5']
 });
