f6d862dbfaa59cd95842733a286f469cd25c3711
William Sargent <wns1997@icloud.com>
2021-04-14 21:34:03 -0400

    filled out Readme

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -40,7 +40,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
       }
       };
     function getradius(mag){
-      return 2*((mag/Math.PI)**.5);
+      return 3*((mag/Math.PI)**.5);
     };
     L.geoJSON(data, {
       style: styleI,
@@ -82,22 +82,17 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
     // a = ['red'. 'blue']
     // b = ['1-3', '3-5']
 });
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(map);
+// var legend = L.control({position: 'bottomright'});
+// legend.onAdd = function (map) {
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         grades = [90, 70, 50, 30, 10, -10],
+//         labels = [];
+//     // loop through our density intervals and generate a label with a colored square for each interval
+//     for (var i = 0; i < grades.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+//     }
+//     return div;
+// };
+// legend.addTo(map);
