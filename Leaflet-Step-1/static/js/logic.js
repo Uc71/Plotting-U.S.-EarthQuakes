91444e3d422e95bb8b1139ef5cfa7450af97c6a5
William Sargent <wns1997@icloud.com>
2021-04-30 21:13:34 -0400

    edited colors for legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -48,39 +48,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
         return L.circleMarker(latlng )
       }
   }).addTo(myMap);
-    //   // Binding a pop-up to each layer
-    //   onEachFeature: function(feature,layer){
-    //     layer.bindPopup("Zip Code: "+feature.properties.ZIP + "<br>Median Household Income:<br>"+
-    //       "$"+feature.properties.MHI2016);
-    //   }
-    // }).addTo(myMap);
-    // Set up the legend
-    // var legend = L.control({ position: "bottomright" });
-    // legend.onAdd = function() {
-    //   var div = L.DomUtil.create("div", "info legend");
-    //   var limits = geojson.options.limits;
-    //   var colors = geojson.options.colors;
-    //   var labels = [];
-    //   // Add min & max
-    //   var legendInfo = "<h1>Median Income</h1>" +
-    //     "<div class=\"labels\">" +
-    //       "<div class=\"min\">" + limits[0] + "</div>" +
-    //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-    //     "</div>";
-    //   div.innerHTML = legendInfo;
-    //   limits.forEach(function(limit, index) {
-    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    //   });
-    //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    //   return div;
-    // };
-    // // Adding legend to the map
-    // legend.addTo(myMap);
-
-
-
-    // a = ['red'. 'blue']
-    // b = ['1-3', '3-5']
 });
 var legend=L.control({
   position:"bottomright"
@@ -90,21 +57,20 @@ legend.onAdd=function(){
   var div=L
     .DomUtil
     .create("div","info legend");
-  var grades=[90,70,50,30,10,-10];
+  var depths=[90,70,50,30,10,-10];
   var colors=[
-    "#ed0053",
-    "#ff7105",
-    "#ffb805",
-    "#edda09",
-    "#f2ff00",
-    "#15ff00"
+    "#FF0101",
+    "#D50000",
+    "#B60000",
+    "#8C0000",
+    "#660000",
+    "#000000"
   ];
   // Loop through our intervals and generate a label with a colored square for each interval.
-  for (var i=0;i<grades.length;i++){
+  for (var i=0;i<depths.length;i++){
     div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
-      grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      depths[i] + (depths[i + 1] ? "&ndash;" + depths[i + 1] + "<br>" : "+");
   }
   return div;
 };
-// We add our legend to the map.
 legend.addTo(myMap);
